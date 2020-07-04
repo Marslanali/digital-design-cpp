943aa620c460bf246efdb9c239b613b8a334fe39
Kaushik <36kashili@gmail.com>
2020-07-03 21:33:34 -0700

    doesnt generate output in ubuntu shell

--- main.cpp
+++ main.cpp
@@ -1,224 +1,59 @@
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <algorithm>
 #include <boost/algorithm/string.hpp>
-#include <fstream>
-#include <iostream>
-#include <iterator>
-#include <memory>
 #include <string>
+#include <iostream>
 #include <vector>
+#include <memory>
+#include <fstream>
 
-// global variables
-int xor_count, and_count, inv_count = 0;
 
-// 64 bits inputs
-std::string input_a = "0010101101100010101010101000101010101001010110110011011110001010";
-std::string input_b = "0000010101101100010101010101000101010101001010110110011011110000";
+int main(int argc, char** argv) { 
+   std::string input1 = "1010101101100010101010101000101010101001010110110011011110001010";
+   std::string input2 = "0000010101101100010101010101000101010101001010110110011011110001";
 
-int main(int argc, char **argv) {
-  // read the circuit data from command line
-  std::ifstream config_file(argv[1]);
-  std::string delimeter_ = " ";
-  std::string line = "";
-  // create vector of vector  of type string
-  std::vector<std::vector<std::string>> data_list;
+   std::string line;
+   std::vector<std::vector<std::string>> gates;
+   std::ifstream input_file(argv[1]);
 
-  /*Iterate through each line and split the content using delimeter*/
-  while (getline(config_file, line)) {
-    std::vector<std::string> vec;
-    boost::algorithm::split(vec, line, boost::is_any_of(delimeter_));
-    data_list.push_back(vec);
-  }
+   while (getline(input_file, line)) {  
+      std::vector<std::string> agate;
+      boost::algorithm::split(agate, line, boost::is_any_of(" "));
+      gates.push_back(agate);
+   }
+ 
+   int num_wires = std::stoi(gates[0][1]);
+  int nnum_input = std::stoi(gates[1][0]);
+   char input1_bits[nnum_input + 1];
+   strcpy(input1_bits, input1.c_str());
+   char input2_bits[nnum_input + 1];
+   strcpy(input2_bits, input2.c_str());
 
-  // print the all data to the console
-  std::cout << std::endl;
-  std::cout << std::endl;
-  std::cout << "\n=========================  Print All Data  ============================\n" << std::endl;
-  for (int i = 0; i < data_list.size(); ++i) {
-    for (int j = 0; j < data_list[i].size(); ++j) {
-      std::cout << data_list[i][j] << " ";
-    }
-    std::cout << "\n" << std::endl;
-  }
+   int wires[num_wires];
+ 
+   for (int temp = 4; temp <= 67; ++temp) {//first, copy input bits into wires (note:circuit's wires go sequentially as inputbits, hence copy sequentially)
+      wires[std::stoi(gates[temp][2])] = input1[temp - 4] - 48;
+      wires[std::stoi(gates[temp][3])] = input2[temp - 4] - 48;
+      //std::cout << wires[std::stoi(gates[temp][2])] << ":" << wires[std::stoi(gates[temp][3])] << std::endl;
+   }
+ 
+   for (int temp = 4; temp < gates.size(); ++temp) {//evaluate each gate
+      //std::cout << temp << "::" << gates[temp][5] << std::endl;
 
-  int total_gates = std::stoi(data_list[0][0]);
-  int total_wires = std::stoi(data_list[0][1]);
-
-  int total_num_input = std::stoi(data_list[1][0]);
-  int inputs_bits = std::stoi(data_list[1][1]);
-
-  int total_num_outputs = std::stoi(data_list[2][0]);
-  int outputs_bits = std::stoi(data_list[2][1]);
-
-  std::cout << "No. of gates: " << total_gates << std::endl;
-  std::cout << "No. of wires: " << total_wires << std::endl;
-
-  std::cout << std::endl;
-  std::cout << std::endl;
-
-  std::cout << "No. of inputs: " << total_num_input << std::endl;
-  std::cout << "No. of input bits: " << inputs_bits << std::endl;
-
-  std::cout << std::endl;
-  std::cout << std::endl;
-
-  std::cout << "No. of outputs: " << total_num_outputs << std::endl;
-  std::cout << "No. of output bits: " << outputs_bits << std::endl;
-
-  std::cout << std::endl;
-  std::cout << std::endl;
-
-  ////////////////////////////////////////////////// Input Data A //////////////////////////////////////////////////////
-  int n = input_a.length();
-
-  // declaring character array
-  char char_array1[n + 1];
-
-  // copying the contents of the
-  // string to char array
-  strcpy(char_array1, input_a.c_str());
-
-  std::cout << "Input A (64 bits): " << std::endl;
-  for (int i = 0; i < n; i++) std::cout << char_array1[i];
-  std::cout << std::endl;
-  std::cout << std::endl;
-
-  ////////////////////////////////////////////////// Input Data B //////////////////////////////////////////////////////
-  int m = input_b.length();
-
-  // declaring character array
-  char char_array2[m + 1];
-
-  // copying the contents of the
-  // string to char array
-  strcpy(char_array2, input_b.c_str());
-
-  std::cout << "Input B (64 bits): " << std::endl;
-  for (int i = 0; i < n; i++) std::cout << char_array2[i];
-  std::cout << std::endl;
-  std::cout << std::endl;
-
-  ////////////////////////////////////////////////// Read Circuit Data /////////////////////////////////////////////////
-  // read all gates data, count gates to be used in next index arrays
-  for (int k = 4; k < data_list.size(); ++k) {
-    // make sure to count only XOR gates
-    if (data_list[k].size() == 6) {
-      // count XOR
-      if (data_list[k][5] == "XOR") {
-        xor_count += 1;
+      if (gates[temp][5] == "XOR") {//?
+         wires[std::stoi(gates[temp][4])] = wires[std::stoi(gates[temp][2])] ^ wires[std::stoi(gates[temp][3])]; 
+         std::cout <<"===xor===="<< gates[temp][4] << std::endl;
       }
-        // count AND
-      else if (data_list[k][5] == "AND") {
-        and_count += 1;
+      else if (gates[temp][5] == "AND") {//?
+         wires[std::stoi(gates[temp][4])] = wires[std::stoi(gates[temp][2])] & wires[std::stoi(gates[temp][3])];
+         std::cout << "===and===="<< gates[temp][4]  << std::endl;
       }
-    }
-      // make sure to count only INV gates
-    else if (data_list[k].size() == 5) {
-      // INV operation
-      if (data_list[k][4] == "INV") {
-        inv_count += 1;
-      }
-    }
-  }
 
-  std::cout << "No. of XOR: " << xor_count << "\n"
-            << "No. of AND: " << and_count << "\n"
-            << "No. of INV: " << inv_count << std::endl;
+   }
+ 
+   for (int temp = num_wires - 1; temp >= num_wires - 64; temp--) {
+      //std::cout << temp;
+      ///std::cout << ":";
+      std::cout << wires[temp];
+   }
+   std::cout << std::endl;
 
-  int wire[total_wires];
-
-  std::cout << std::endl;
-  std::cout << std::endl;
-  std::cout << std::endl;
-
-  //////////////////////////////////////////////// Copy Input A and B //////////////////////////////////////////////////
-  // gates index and save them in respective arrays start from here
-  for (int k = 4; k <= 67; ++k) {
-    // check XOR and AND gates only
-    if (data_list[k].size() == 6) {
-      // XOR data to be saved in array
-      if (data_list[k][5] == "XOR") {
-        // Copy inputs A
-        wire[std::stoi(data_list[k][2])] = char_array1[k - 4] - 48;
-        // Copy inputs B
-        wire[std::stoi(data_list[k][3])] = char_array2[k - 4] - 48;
-      }
-    }
-  }
-
-  std::cout << std::endl;
-  std::cout << std::endl;
-  std::cout << std::endl;
-
-  ////////////////////////////////////////// 64-bit Subtractor Start From Here /////////////////////////////////////////
-  // gates index and save them in respective arrays start from here
-  for (int k = 4; k < data_list.size(); ++k) {
-    // check XOR and AND gates only
-    std::cout << std::endl;
-    std::cout << "\n=========================  XOR/AND BLOCKS # " << k - 4 << "  ============================\n"
-              << std::endl;
-    if (data_list[k].size() == 6) {
-      // XOR data to be saved in array
-      if (data_list[k][5] == "XOR") {
-        // stores every gates wires in respective arrays
-        wire[std::stoi(data_list[k][4])] = wire[std::stoi(data_list[k][2])] ^ wire[std::stoi(data_list[k][3])];
-        std::cout << "Wires: : " << std::stoi(data_list[k][2]) << " " << std::stoi(data_list[k][3]) << " "
-                  << std::stoi(data_list[k][4]) << " " << std::endl;
-        std::cout << "XOR GATE : " << wire[std::stoi(data_list[k][2])] << " " << wire[std::stoi(data_list[k][3])] << " "
-                  << wire[std::stoi(data_list[k][4])] << " " << std::endl;
-
-      }
-        // check INV gate only
-      else if (data_list[k][5] == "AND") {
-        // stores every gates wires index in respective arrays
-        wire[std::stoi(data_list[k][4])] = wire[std::stoi(data_list[k][2])] & wire[std::stoi(data_list[k][3])];
-        std::cout << "Wires: : " << std::stoi(data_list[k][2]) << " " << std::stoi(data_list[k][3]) << " "
-                  << std::stoi(data_list[k][4]) << " " << std::endl;
-        std::cout << "AND GATE : " << wire[std::stoi(data_list[k][2])] << " " << wire[std::stoi(data_list[k][3])] << " "
-                  << wire[std::stoi(data_list[k][4])] << " " << std::endl;
-      }
-    }
-      // check INV gate only
-    else if (data_list[k].size() == 5) {
-      // INV gate data to be store in array
-      if (data_list[k][4] == "INV") {
-        wire[std::stoi(data_list[k][3])] = !wire[std::stoi(data_list[k][2])];
-        std::cout << "Wires: : " << std::stoi(data_list[k][2]) << " " << std::stoi(data_list[k][3]) << " " << std::endl;
-        std::cout << "INV GATE : " << wire[std::stoi(data_list[k][2])] << " " << wire[std::stoi(data_list[k][3])] << " "
-                  << std::endl;
-      }
-    }
-  }
-
-  std::cout << std::endl;
-
-  ////////////////////////////////////////////////// Final Output //////////////////////////////////////////////////////
-  std::vector<int> ind_xor_final;
-  int counter = 0;
-  int n_elem = sizeof(wire)/sizeof(wire[0]);
-
-  for (int l = n_elem-1; l > 0; --l) {
-    ind_xor_final.push_back(l);
-    counter += 1;
-    if (counter >= 64)
-      break;
-  }
-
-  for (std::vector<int>::iterator it = ind_xor_final.begin(); it != ind_xor_final.end(); ++it) {
-    std::cout << *it << " ";
-  }
-
-  std::cout << std::endl;
-  std::cout << std::endl;
-
-  for (std::vector<int>::iterator it = ind_xor_final.begin(); it != ind_xor_final.end(); ++it) {
-    std::cout << wire[*it];
-  }
-  std::cout << std::endl;
-  std::cout << std::endl;
-  /*Close the File*/
-  config_file.close();
-  return 0;
 }
