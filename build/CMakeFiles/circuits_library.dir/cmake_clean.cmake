1cf12a0dda8e801e8b88d4e522aa0a52c762864e
marslanali <arslanali800@hotmail.com>
2020-07-17 14:31:09 +0500

    changes push after first cleanup

---
+++ build/CMakeFiles/circuits_library.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/circuits_library.dir/shared-lib/src/Circuits.cpp.o"
+  "CMakeFiles/circuits_library.dir/shared-lib/src/ReadData.cpp.o"
+  "libcircuits_library.pdb"
+  "libcircuits_library.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/circuits_library.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
