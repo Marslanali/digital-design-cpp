26db090b6ca8be26500430923c0f13fb4e33ba97
marslanali <arslanali800@hotmail.com>
2020-07-26 18:28:27 +0500

    AES Completed

--- main.cpp
+++ main.cpp
@@ -10,12 +10,7 @@
 #include "../digitaldesign/include/Constants.h"
 #include "../digitaldesign/include/ReadData.h"
 
-/*std::string key =
-"00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
-std::string message =
-"11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111";*/
-
-void test_arithmetic(std::string file_path, int input_a_size, const int input_b_size, const int output_size) {
+void test_arithmetic(const std::string file_path, const int input_a_size, const int input_b_size, const int output_size) {
   ////////////////////////////////////// Arithmetic Functions ///////////////////////////////////////////////////
   // Create and instance of ReadData class and called constructor
   ReadData text_file_read(file_path);
@@ -25,69 +20,75 @@ void test_arithmetic(std::string file_path, int input_a_size, const int input_b_
 
   // Create an instance of Circuit class
   Circuits* circuit_obj = new Circuits;
+  // std::shared_ptr<Circuits> circuit_obj = std::make_shared<Circuits>();
 
   // circuit_obj calling to its member function i.e display_circuit
   circuit_obj->display_circuit(data_list);
 
-  // Key
-  std::string key(input_a_size, 'x');
-  // Message
-  std::string message(input_b_size, 'x');
+  if (input_b_size == 0) {
+    // Key
+    std::string key1(1600, 'x');
+    // Key
+    for (int i = 0; i < 1600; ++i) {
+      key1[i] = '0';
+    }
 
-  // Key
-  for (int i = 0; i < input_a_size; ++i) {
-    key[i] = '0';
+    // Input A 64-bits
+    char* char_array1 = circuit_obj->read_inputs_A(key1);
+
+    std::vector<unsigned int> return_wires = circuit_obj->arithmetic_functions(data_list, char_array1, NULL, input_a_size, input_b_size);
+    circuit_obj->display_output(return_wires, output_size);
+
+    delete circuit_obj;
+
   }
 
-  // Message
-  for (int i = 0; i < input_b_size; ++i) {
-    message[i] = '1';
+  else {
+    // Key
+    std::string key(input_a_size, 'x');
+    // Key
+    for (int i = 0; i < input_a_size; ++i) {
+      key[i] = '0';
+    }
+
+    // Message
+    std::string message(input_b_size, 'x');
+
+    // Message
+    for (int i = 0; i < input_b_size; ++i) {
+      message[i] = '1';
+    }
+
+    // Input A and B 64-bits
+    //    const char* char_array1 = circuit_obj->read_inputs_A(key);
+    //    char* char_array2 = circuit_obj->read_inputs_B(message);
+    /*
+         std::vector<unsigned int> return_wires =
+             circuit_obj->arithmetic_functions(data_list, char_array1, char_array2, input_a_size, input_b_size);
+
+         circuit_obj->display_output(return_wires, output_size);*/
   }
-
-  /*
-  if (file_path == "../data/arithmetic-functions/Keccak_f.txt")
-  {
-      // Input A 64-bits
-      char* char_array1 = circuit_obj->read_inputs_A(input_single);
-
-      std::vector<unsigned int> return_wires = circuit_obj->arithmetic_functions(data_list, char_array1, NULL);
-      circuit_obj->display_output(return_wires, output_size);
-  }
-
-  else
-  {*/
-
-  // Input A and B 64-bits
-  char* char_array1 = circuit_obj->read_inputs_A(key);
-  char* char_array2 = circuit_obj->read_inputs_B(message);
-
-  std::vector<unsigned int> return_wires =
-      circuit_obj->arithmetic_functions(data_list, char_array1, char_array2, input_a_size, input_b_size);
-
-  circuit_obj->display_output(return_wires, output_size);
-
-  delete circuit_obj;
 }
 
 int main(int argc, char** argv) {
   try {
-
-    std::cout << "================== Testing a AES-128 Circuit ======================" << std::endl;
+  /*  std::cout << "================== Testing a AES-128 Circuit ======================" << std::endl;
     test_arithmetic("../data/cryptographic-functions/aes_128.txt", 128, 128, 128);
-    /* std::cout << "================== Testing aes_192 ======================== " << std::endl;
-     test_arithmetic("../data/cryptographic-functions/aes_192.txt", 192, 128, 128);*/
 
-    /*        std::cout << "================== Testing aes_256 ========================" << std::endl;
-    test_arithmetic("../data/arithmetic-functions/aes_256.txt", 128);
+    std::cout << "================== Testing aes_192 ======================== " << std::endl;
+    test_arithmetic("../data/cryptographic-functions/aes_192.txt", 192, 128, 128);
 
-    std::cout << "================== Testing Keccak_f ========================" << std::endl;
-    test_arithmetic("../data/arithmetic-functions/Keccak_f.txt", 1600);
+    std::cout << "================== Testing aes_256 ========================" << std::endl;
+    test_arithmetic("../data/cryptographic-functions/aes_256.txt", 256, 128, 128);*/
 
-    std::cout << "================== Testing sha256 ========================== " << std::endl;
-    test_arithmetic("../data/arithmetic-functions/sha256.txt", 256);
-
-    std::cout << "================== Testing sha512.txt ========================== " << std::endl;
-    test_arithmetic("../data/arithmetic-functions/sha512.txt", 512);*/
+  /*  std::cout << "================== Testing Keccak_f ========================" << std::endl;
+    test_arithmetic("../data/cryptographic-functions/Keccak_f.txt", 1600, 0, 1600);
+*/
+       /*  std::cout << "================== Testing sha256 ========================== " << std::endl;
+         test_arithmetic("../data/arithmetic-functions/sha256.txt", 512,256,256);
+*/
+/*         std::cout << "================== Testing sha512.txt ========================== " << std::endl;
+         test_arithmetic("../data/arithmetic-functions/sha512.txt", 512);*/
   }
 
   catch (const std::runtime_error& ex) {
--- src/Circuits.cpp
+++ src/Circuits.cpp
@@ -15,16 +15,6 @@ void Circuits::display_circuit(const std::vector<std::vector<std::string>>& data
   int inv_count_ = 0;
   // print the all data to the console
   std::cout << std::endl;
-  /* std::cout << std::endl;
-  std::cout << "\n=========================  Print All Data  ============================\n" << std::endl;
-  for (int i = 0; i < data_list.size(); ++i)
-  {
-      for (int j = 0; j < data_list[i].size(); ++j)
-      {
-          std::cout << data_list[i][j] << " ";
-      }
-      std::cout << "\n" << std::endl;
-  }*/
 
   total_gates_ = std::stoi(data_list[0][0]);
   total_wires_ = std::stoi(data_list[0][1]);
@@ -53,42 +43,6 @@ void Circuits::display_circuit(const std::vector<std::vector<std::string>>& data
   std::cout << std::endl;
   std::cout << std::endl;
 
-  /*    ////////////////////////////////////////// Read Circuit Total Gates
-  /////////////////////////////////////////////////
-  // read all gates data, count gates to be used in next index arrays
-  for (int k = 4; k < data_list.size(); ++k)
-  {
-      // make sure to count only XOR gates
-      if (data_list[k].size() == 6)
-      {
-          // count XOR
-          if (data_list[k][5] == "XOR")
-          {
-              xor_count_ += 1;
-          }
-          // count AND
-          else if (data_list[k][5] == "AND")
-          {
-              and_count_ += 1;
-          }
-      }
-      // make sure to count only INV gates
-      else if (data_list[k].size() == 5)
-      {
-          // INV operation
-          if (data_list[k][4] == "INV")
-          {
-              inv_count_ += 1;
-          }
-      }
-  }
-
-    std::cout << "No. of XOR: " << xor_count_ << "\n"
-            << "No. of AND: " << and_count_ << "\n"
-            << "No. of INV: " << inv_count_ << std::endl;
-
-  std::cout << std::endl;
-  std::cout << std::endl;*/
 }
 
 // Read inputs A
@@ -138,7 +92,7 @@ std::vector<unsigned int> Circuits::arithmetic_functions(const std::vector<std::
 
   //////////////////////////////////////////////// Copy Input A and B ////////////////////////////////////////////////
   // gates index and save them in respective arrays start from here
-  for (int k = 0; k <= 127; ++k) {
+  for (int k = 0; k <= input_a_size - 1; ++k) {
     // Copy inputs B
     wires[k] = *char_array1 - 48;
     // increment pointer for next element fetch
@@ -146,7 +100,7 @@ std::vector<unsigned int> Circuits::arithmetic_functions(const std::vector<std::
   }
 
   if (char_array2 != NULL) {
-    for (int k = 128; k <= 255; ++k) {
+    for (int k = input_a_size; k <= input_a_size + input_b_size - 1; ++k) {
       // Copy inputs A
       wires[k] = *char_array2 - 48;
       // increment pointer for next element fetch
@@ -240,48 +194,16 @@ void Circuits::display_output(std::vector<unsigned int> wires_temp, int output_b
   }
 
   else if (output_bit_size == 128) {
-    /*  std::vector<unsigned int> index1;
-    int counter1 = 0;
-
-    for (unsigned int l = total_wires_ - 1; l > 0; --l)
-    {
-        index1.push_back(l);
-        counter1 += 1;
-        if (counter1 >= 64)
-            break;
-    }
-
-    std::vector<unsigned int> index2;
-    int counter2 = 0;
-
-    for (unsigned int l = total_wires_ - 65; l > 0; --l)
-    {
-        index2.push_back(l);
-        counter2 += 1;
-        if (counter2 >= 64)
-            break;
-    }*/
 
     std::cout << std::endl;
     std::cout << "Output " << output_bit_size << " bits: " << std::endl;
-    /*
 
-    for (std::vector<unsigned int>::iterator it = index2.begin(); it != index2.end(); ++it)
-    {
-        std::cout << wires_temp[*it];
-    }
-
-    for (std::vector<unsigned int>::iterator it = index1.begin(); it != index1.end(); ++it)
-    {
-        std::cout << wires_temp[*it];
-    }
-*/
     std::vector<unsigned int> index;
     int counter = 0;
     int num = 0;
     int byte;
 
-    for (unsigned int l = total_wires_ - 1; l > 0; --l) {
+    for (unsigned int l = total_wires_ - output_bit_size; l <= total_wires_ - 1; ++l) {
       index.push_back(l);
       counter += 1;
       if (counter >= output_bit_size) break;
@@ -300,19 +222,29 @@ void Circuits::display_output(std::vector<unsigned int> wires_temp, int output_b
     }
     std::cout << std::endl;
 
-    /*    for (int i= 15; i >= 0; i--)
-    {
-        byte= 0;
-        for (int j= 7; j >= 0; j--)
-        {
-            byte<<= 1;
-            byte+=  wires_temp[*it];
-        }
-        printf("%02X", byte);
+
+    std::cout << std::endl;
+  }
+
+  else if (output_bit_size == 1600) {
+    std::cout << std::endl;
+    std::cout << "Output " << output_bit_size << " bits: " << std::endl;
+
+    std::vector<unsigned int> index;
+    int counter = 0;
+    int num = 0;
+    int byte;
+
+    for (unsigned int l = total_wires_ - output_bit_size; l <= total_wires_ - 1; ++l) {
+      index.push_back(l);
+
+      counter += 1;
+      if (counter >= output_bit_size) break;
     }
 
-*/
-
+    for (std::vector<unsigned int>::iterator it = index.begin(); it != index.end(); ++it) {
+      std::cout << wires_temp[*it];
+    }
     std::cout << std::endl;
   }
 }
